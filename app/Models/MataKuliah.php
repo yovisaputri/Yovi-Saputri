8ab181bc9045b9313db719b97c95ad840bb4c789
yovisaputri <yovisaputri3003@gmail.com>
2023-12-22 09:13:12 +0700

    Final Project yovisaputri

---
+++ app/Models/MataKuliah.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MataKuliah extends Model
+{
+    use HasFactory;
+    protected $table = 'matakuliahs';
+    protected $fillable = [
+        'kode_mk',
+        'nama_mk',
+        'sks',
+        'semester',
+		];
+
+}
